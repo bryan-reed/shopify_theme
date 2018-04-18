e97b4371a555c57cfe371fb33253eb625a390645
bryan-reed-88 <bryanreed@Commerce-MacBook-Pro.local>
2018-04-18 07:30:02 -0700

    Initial mobile nav functionality, image grid update to match mockup options

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -3,14 +3,16 @@ import $ from 'jquery';
 class MobileMenu {
     constructor() {
         this.menuIcon = $('.main-header__menu-icon');
-        this.menu = $('.main-header__nav__top');
+        this.menu = $('.main-header__nav');
+        this.moveOver = $('.move-over');
         this.events();
     }
     events() {
         this.menuIcon.click(this.toggleMenu.bind(this));
     }
     toggleMenu() {
-        this.menu.toggleClass('main-header__nav__top--open');
+        this.menu.toggleClass('main-header__nav--open');
+        this.moveOver.toggleClass('open');
         this.menuIcon.toggleClass('main-header__menu-icon--close-x')
     }
 }
